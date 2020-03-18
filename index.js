32be9b04997cb4ee6fd59d0d70aa286bb867dc42
jbeasant1 <jayendrasinghchandel@gmail.com>
2020-03-18 04:07:03 +0000

    web_proj_wid_Redis

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const redis = require("redis");
+const process = require("process");
+
+const app = express();
+const client = redis.createClient({
+  host: "redis-server",
+  port: 6379
+});
+client.set("visits", 0);
+
+app.get("/", (req, res) => {
+  //process.exit(0)
+  client.get("visits", (err, visits) => {
+    res.send("Jay, you have this many number of visits " + visits);
+    client.set("visits", parseInt(visits) + 1);
+  });
+});
+
+app.listen(8082, () => {
+  console.log("listening on port 8082");
+});
+
